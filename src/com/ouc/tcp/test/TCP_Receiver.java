656d16c4ffaa648ef0d9f8230dc0a386c1dd09b6
Mao-uc <3339527188@qq.com>
2025-12-13 02:55:06 +0800

    Changed the eFlag to adapt to RDT2.0.

--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -15,8 +15,7 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	private TCP_PACKET ackPack; // ACK packet to be replied
 	int sequence = 1;// Used to record the sequence number of the current packet to be received, note
 						// that the packet sequence number is not completely
-	int lastack = -1;
-	
+
 	/* Constructor */
 	public TCP_Receiver() {
 		super(); // Call superclass constructor
@@ -28,21 +27,19 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	public void rdt_recv(TCP_PACKET recvPack) {
 		// Check checksum, generate ACK
 		if (CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
-
-			int sequence_cur = recvPack.getTcpH().getTh_seq();
-			
 			// Generate ACK packet (set acknowledgment number)
-			tcpH.setTh_ack(sequence_cur);
+			tcpH.setTh_ack(recvPack.getTcpH().getTh_seq());
 			ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
 			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
 			// Reply ACK packet
 			reply(ackPack);
-			
+
+			int sequence_cur = recvPack.getTcpH().getTh_seq();
+
 			if (sequence_cur == sequence) {
 				// Insert the correctly received and ordered data into the data queue, ready for
 				// delivery
 				dataQueue.add(recvPack.getTcpS().getData());
-				lastack = sequence;
 				sequence += recvPack.getTcpS().getData().length;
 			} else {
 				System.out.println("Duplicate packet, Seq =" + recvPack.getTcpH().getTh_seq());
@@ -52,7 +49,7 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 			System.out.println("Recieved Packet" + recvPack.getTcpH().getTh_sum());
 			System.out
 					.println("Problem: Packet Number: " + recvPack.getTcpH().getTh_seq() + " + InnerSeq:  " + sequence);
-			tcpH.setTh_ack(lastack);
+			tcpH.setTh_ack(-1);
 			ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
 			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
 			// Reply ACK packet
@@ -98,7 +95,7 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	// Reply ACK packet
 	public void reply(TCP_PACKET replyPack) {
 		// Set error control flag
-		tcpH.setTh_eflag((byte) 1); // eFlag=0, channel has no error
+		tcpH.setTh_eflag((byte) 0); // eFlag=0, channel has no error
 
 		// Send packet
 		client.send(replyPack);
